4727d43e182cd9e91ab8ba529f6d9310704e0dfb
pdn <pnathan@vandals.uidaho.edu>
2013-02-21 23:32:52 -0800

    1/3 done with the cl-rainbow port.
    - Still need to integrate effects
    
    - Still need 256-color support
    
    - Still need *enable* enabled
    
    - Still need the n-way mapping from color-spec to code.
    
    And some tests.

--- cl-ansi-text.lisp
+++ cl-ansi-text.lisp
@@ -10,7 +10,10 @@
   (:export
    #:with-color
    #:make-color-string
-   #:+resetp-color-string+
+   #:+reset-color-string+
+   #:+term-colors+
+   #:+term-effects+
+   #:*enabled
    ))
 (in-package :cl-ansi-text)
 
@@ -26,6 +29,61 @@
        (defmethod make-load-form ((obj cl-colors:rgb )  &optional env)
 	 (make-load-form-saving-slots obj)))
 
+(defparameter *enabled* t
+  "Turns on/off the colorization of functions" )
+
+(defparameter +reset-color-string+
+  (concatenate 'string (list (code-char 27) #\[ #\0 #\m))
+  "This string will reset ANSI colors")
+
+(defvar +cl-bright-colors+
+	(vector
+         cl-colors:+black+
+         cl-colors:+red+
+         cl-colors:+green+
+         cl-colors:+yellow+
+         cl-colors:+blue+
+         cl-colors:+magenta+
+         cl-colors:+cyan+
+         cl-colors:+white+)
+  "CL-COLORS bright colors")
+
+(defvar +cl-colors+
+  (vector
+   cl-colors:+darkgrey+
+   cl-colors:+darkred+
+   cl-colors:+darkgreen+
+   cl-colors:+wheat+ 	;kind of darkyellow
+   cl-colors:+darkblue+
+   cl-colors:+darkmagenta+
+   cl-colors:+darkcyan+
+   cl-colors:+grey+)
+  "CL-COLORS colors")
+
+(defparameter +term-colors+
+  '((:black . 0)
+    (:red . 1)
+    (:green . 2)
+    (:yellow . 3)
+    (:blue . 4)
+    (:magenta . 5)
+    (:cyan . 6)
+    (:white . 7)
+    (:default . 9))
+  "Basic colors")
+
+(defparameter +term-effects+
+  '((:normal . nil)                     ;the usual
+    (:reset . 0)
+    (:bright . 1)
+    (:italic . 3)
+    (:underline . 4)
+    (:blink . 5)
+    (:inverse . 7)
+    (:hide . 8))
+  "ANSI terminal effects")
+
+(defparameter +color-map+)
 
 (defun explode (input)
   "Assumes input is a vector, returns it as a list"
@@ -39,33 +97,7 @@
 	  #'alexandria:ensure-list
 	  items) ))
 
-
-(defparameter +reset-color-string+
-  (concatenate 'string (list (code-char 27) #\[ #\0 #\m)))
-
-(defvar +bright-colors+
-	(vector
-         cl-colors:+black+
-         cl-colors:+red+
-         cl-colors:+green+
-         cl-colors:+yellow+
-         cl-colors:+blue+
-         cl-colors:+magenta+
-         cl-colors:+cyan+
-         cl-colors:+white+))
-
-(defvar +colors+
-  (vector
-   cl-colors:+darkgrey+
-   cl-colors:+darkred+
-   cl-colors:+darkgreen+
-   cl-colors:+wheat+ 	;kind of darkyellow
-   cl-colors:+darkblue+
-   cl-colors:+darkmagenta+
-   cl-colors:+darkcyan+
-   cl-colors:+grey+))
-
-(defun ansi-to-cl-colors (color-code &optional bright)
+(defun keyword-to-cl-colors (color-code &optional bright)
   (aref (if bright
             +bright-colors+
             +colors+) color-code))
@@ -108,7 +140,7 @@
     (cl-colors:rgb (cl-colors-to-ansi color))
     (list color)))
 
-(defun build-control-string (color)
+(defun build-control-string (color effect)
   "Build the basic control character list"
   (let ((codes (mapcar #'(lambda (n)
 			  (explode (write-to-string n)))
@@ -117,13 +149,13 @@
 	(car codes)
 	(extend (first codes) #\; (second codes)))))
 
-(defun make-color-string (color)
+(defun make-color-string (color-spec &optional (effect nil))
   "Takes either a cl-color or a list denoting the ANSI colors and
 returns a string sufficient to change to the given color"
   (concatenate 'string
 	       (append
 		`( ,(code-char 27) #\[)
-		(build-control-string color)
+		(build-control-string color effect)
 		'(#\m))))
 
 (defmacro with-color ((color &key (stream t))
@@ -146,6 +178,55 @@ then writes out the string denoting a `reset`."
 			 +reset-color-string+)))
        (format ,dest ,sym ,args))))
 
+
+
+;;; http://www.frexx.de/xterm-256-notes/
+(defun rgb-to-ansi (red green blue)
+  (let ((ansi-domain (mapcar #'(lambda (x)
+                                 (floor (* 6 (/ x 256.0))))
+                             (list red green blue))))
+    (+ 16
+     (* 36 (first ansi-domain))
+     (* 6 (second ansi-domain))
+     (third ansi-domain))))
+
+
+(defun code-from-rgb (style red green blue)
+  (format nil "~d;5;~d" (if (eql style :foreground) 38 48)
+          (rgb-to-ansi red green blue)))
+
+(defgeneric ansi-color-code (style color-argument)
+  (:documentation
+   "Returns the ANSI color code indicated by color-argument, modified by style."))
+
+(defmethod ansi-color-code (style (color symbol))
+  (let ((col (cdr  (assoc color +term-colors+)))
+        (ground (if (eql style :foreground) 30 40)))
+    (if col (+ col ground) 0)))
+
+
+(defmethod ansi-color-code (style (color integer))
+  (code-from-rgb style
+                 ;; classic bitmask
+                 (ash  (logand color #xff0000) -16)
+                 (ash  (logand color #x00ff00) -8)
+                 (logand color #x0000ff)))
+
+(defmethod ansi-color-code (style (color list))
+  (unless (consp list)
+    (error "~a must be a three-integer list"))
+
+  (unless (and (integerp (first color))
+               (integerp (second color))
+               (integerp (second color)))
+    (error "~a must have three integers"))
+
+  (code-from-rgb style
+                 (first color)
+                 (second color)
+                 (third color)))
+
+(deftype )
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;; ~/.../ functions.
 ;;; Suggested by H4ns
