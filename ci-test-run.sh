89af01a6c4d9527a9f1d90f1bb177f3013222409
pnathan <pnathan@vandals.uidaho.edu>
2014-12-31 13:50:17 -0800

    Setting up for Travis CI

--- ci-test-run.sh
+++ ci-test-run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 echo "CI run using SBCL"
-sbcl --eval "(ql:quickload '(:cl-colors :alexandria :fiveam))" --load cl-ansi-text.lisp --load test/cl-ansi-text-test.lisp --eval "(cl-ansi-text-test::ci-run)"  --non-interactive
+sbcl --script travis-tests.lisp
--- test/cl-ansi-text-test.lisp
+++ test/cl-ansi-text-test.lisp
@@ -4,8 +4,8 @@
 (defpackage :cl-ansi-text-test
   (:use :common-lisp
         :cl-user
-	:cl-ansi-text
-	:fiveam))
+        :cl-ansi-text
+        :fiveam))
 
 (in-package :cl-ansi-text-test)
 (use-package :fiveam)
@@ -20,50 +20,49 @@
 (test basic-color-strings
   "Test the basic stuff"
   (is (equal '(#\Esc #\[ #\3 #\1 #\m)
-	     (cl-ansi-text::build-control-string :red :unset :foreground)))
+             (cl-ansi-text::build-control-string :red :unset :foreground)))
   (is (equal '(#\Esc #\[ #\4 #\1 #\m)
-	     (cl-ansi-text::build-control-string :red :unset :background)))
+             (cl-ansi-text::build-control-string :red :unset :background)))
   (is (equal '(#\Esc #\[ #\4 #\2 #\; #\1 #\m)
-	     (cl-ansi-text::build-control-string :green :bright :background))))
+             (cl-ansi-text::build-control-string :green :bright :background))))
 
 (test enabled-connectivity
   "Test *enabled*'s capability"
  (is (equal  '(#\Esc #\[ #\3 #\1 #\m)
-	     (let ((*enabled* t))
-	       (concatenate
-		'list
-		(cl-ansi-text:make-color-string :red)))))
+             (let ((*enabled* t))
+               (concatenate
+                'list
+                (cl-ansi-text:make-color-string :red)))))
  (is (equal  '()
-	    (let ((*enabled* nil))
-	      (concatenate
-	       'list
-	       (cl-ansi-text:make-color-string :red)))))
+            (let ((*enabled* nil))
+              (concatenate
+               'list
+               (cl-ansi-text:make-color-string :red)))))
  (is (equal "hi"
-	    (let ((*enabled* nil))
-	      (with-output-to-string (s)
-		(with-color (:red :stream s) (format s "hi"))))))
+            (let ((*enabled* nil))
+              (with-output-to-string (s)
+                (with-color (:red :stream s) (format s "hi"))))))
  (is (equal '(#\Esc #\[ #\3 #\1 #\m #\T #\e #\s #\t #\! #\Esc #\[ #\0 #\m)
-	    (concatenate
-	     'list
-	     (with-output-to-string (s)
-	       (with-color (:red :stream s)
-		 (format s "Test!")))))))
+            (concatenate
+             'list
+             (with-output-to-string (s)
+               (with-color (:red :stream s)
+                 (format s "Test!")))))))
 
 (test rgb-suite
   "Test RGB colors"
   (is (equal '(#\Esc #\[ #\3 #\8 #\; #\5 #\; #\2 #\1 #\4 #\m)
-	     (cl-ansi-text::build-control-string #xFFAA00
-						 :unset :foreground)))
+             (cl-ansi-text::build-control-string #xFFAA00
+                                                 :unset :foreground)))
   (is (equal '(#\Esc #\[ #\4 #\8 #\; #\5 #\; #\2 #\1 #\4 #\m)
-	     (cl-ansi-text::build-control-string #xFFAA00
-						 :unset :background)))
+             (cl-ansi-text::build-control-string #xFFAA00
+                                                 :unset :background)))
   (is (equal '(#\Esc #\[ #\4 #\8 #\; #\5 #\; #\1 #\6 #\m)
-	     (cl-ansi-text::build-control-string #x000000
-						 :unset :background)))
+             (cl-ansi-text::build-control-string #x000000
+                                                 :unset :background)))
   (is (equal '(#\Esc #\[ #\4 #\8 #\; #\5 #\; #\2 #\3 #\1 #\m)
-	     (cl-ansi-text::build-control-string #xFFFFFF
-						 :unset :background)))
-  )
+             (cl-ansi-text::build-control-string #xFFFFFF
+                                                 :unset :background))))
 
 (test color-named-functions
   (let ((str "Test string."))
