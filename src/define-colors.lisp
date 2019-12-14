4812381a9b8edee2c8f480c02f5e782ca371c786
Masataro Asai <guicho2.71828@gmail.com>
2019-12-13 21:46:47 -0500

    fix typo

--- src/define-colors.lisp
+++ src/define-colors.lisp
@@ -6,7 +6,7 @@
      ,@(map 'list
             (lambda (color)
               `(defun ,(intern (symbol-name color)) (string &key (effect :unset) (style :foreground))
-                 ,(format nil "Returns a string decolated in ~(~a~)." color)
+                 ,(format nil "Returns a string decorated in ~(~a~)." color)
                  (with-output-to-string (s)
                    (with-color (,color :stream s :effect effect :style style)
                      (write-string string s)))))
